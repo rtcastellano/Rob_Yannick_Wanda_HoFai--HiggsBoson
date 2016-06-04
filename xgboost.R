212904bc4ef02a433e418b136e16f64668a70c7f
rtcastellano <RTCastellano@gmail.com>
2016-06-04 17:24:46 -0400

    correcting xgboost code

--- xgboost.R
+++ xgboost.R
@@ -43,12 +43,9 @@ scaled.weight = higgs.weight * nrow(higgs.test)/length(higgs.labels)
 higgs.train = higgs.train[, -c(1,32,33)]
 higgs.test <- higgs.test[,-1]
 
-#Weighted sum of the signal/background data
-sumwpos <- sum(scaled.weight * (higgs.labels=='X0'))
-sumwneg <- sum(scaled.weight * (higgs.labels=='X1'))
-
 #Create a dummy variable for the "PRI_jet_num" variable
 higgs.train.dummy = dummy.data.frame(higgs.train, names = "PRI_jet_num")
+higgs.test.dummy = dummy.data.frame(higgs.test, names = "PRI_jet_num")
 
 #############################################
 ##############################################
@@ -89,6 +86,17 @@ xgb_train_1 = train(
   weights = scaled.weight
 )
 
+###############################
+#Best parameters of first grid search:
+#eta = .5
+#max_depth = 4
+#nrounds = 100
+#gamma = 0
+#colsample_bytree = .7
+#min_child_weight = 1
+###############################
+
+#Second grid search
 xgb_grid_2 = expand.grid(
   eta = c(.4, .5, .6),                #[2-10]/num trees
   max_depth = c(3, 4, 5),             #Start with 6
@@ -110,47 +118,135 @@ xgb_train_2 = train(
   weights = scaled.weight
 )
 
+############################################
+#Best parameters of second grid search:
+#eta = .4
+#gamma = 0
+#max_depth = 5
+#colsample_bytree = .8
+#min_child_weight = 1
+###########################################
 
+#Third grid search
+xgb_grid_3 = expand.grid(
+  eta = c(.2, .3, .4, .45),                #[2-10]/num trees
+  max_depth = 5,             #Start with 6
+  nrounds = 100,                      #Fix at 100
+  gamma = 0,                          #Usually ok to leave at 0
+  colsample_bytree = c(.75, .8, .85),   #.3 - .5
+  min_child_weight = 1                #start with 1/sqrt(eventrate)
+)
+
+xgb_train_3 = train(
+  x = higgs.train.dummy,
+  y = higgs.labels,
+  trControl = xgb_trcontrol_1,
+  tuneGrid = xgb_grid_3,
+  method = "xgbTree",
+  na.action = na.pass,
+  missing = NA,
+  metric = "ROC",
+  weights = scaled.weight
+)
+
+###########################################
+#Best parameters for thrird grid search:
+#eta = .2
+#gamma = 0
+#max_depth = 5
+#colsample_bytree = .85
+#min_child_weight = 1
+########################################
+
+#Fourth grid search
+xgb_grid_4 = expand.grid(
+  eta = c(.01, .05, .1, .15, .2, .25),                #[2-10]/num trees
+  max_depth = 5,             #Start with 6
+  nrounds = 200,                      #Fix at 100
+  gamma = 0,                          #Usually ok to leave at 0
+  colsample_bytree = .85,   #.3 - .5
+  min_child_weight = 1                #start with 1/sqrt(eventrate)
+)
+
+xgb_train_4 = train(
+  x = higgs.train.dummy,
+  y = higgs.labels,
+  trControl = xgb_trcontrol_1,
+  tuneGrid = xgb_grid_4,
+  method = "xgbTree",
+  na.action = na.pass,
+  missing = NA,
+  metric = "ROC",
+  weights = scaled.weight
+)
+
+#########################################
+#Best parameters for fourth grid search:
+#nrounds - 200
+#eta = .2
+#gamma = 0
+#max_depth = 5
+#colsample_bytree = .85
+#min_child_weight = 1
+########################################
+
+#Five grid search
+xgb_grid_5 = expand.grid(
+  eta = .2,                #[2-10]/num trees
+  max_depth = 5,             #Start with 6
+  nrounds = c(100, 200, 400, 500, 1000),                      #Fix at 100
+  gamma = 0,                          #Usually ok to leave at 0
+  colsample_bytree = .85,   #.3 - .5
+  min_child_weight = 1                #start with 1/sqrt(eventrate)
+)
+
+xgb_train_5 = train(
+  x = higgs.train.dummy,
+  y = higgs.labels,
+  trControl = xgb_trcontrol_1,
+  tuneGrid = xgb_grid_5,
+  method = "xgbTree",
+  na.action = na.pass,
+  missing = NA,
+  metric = "ROC",
+  weights = scaled.weight
+)
+
+#nrounds = 200 is still the best
 
 
 # scatter plot of the AUC against max_depth and eta
-ggplot(xgb_train_1$results, aes(x = as.factor(eta), y = max_depth, size = ROC, color = ROC)) + 
-  geom_point() + 
-  theme_bw() + 
+# ggplot(xgb_train_5$results, aes(x = as.factor(nrounds), y = colsample_bytree, size = ROC, color = ROC)) + 
+#   geom_point() + 
+#   theme_bw() + 
   scale_size_continuous(guide = "none")
 #######################################
-xgb_train_1
-xgb_train_1$bestTune
-xgb_train_1$finalModel
+#Predictions
+######################################
+bst = xgb_train_5$finalModel
+importance_matrix = xgb.importance(feature_names = colnames(higgs.train.dummy), model = bst)
 
-
-
-
-
-###################################################
-###################################################
-
-
+#Predicting training data
 xgmat.train <- xgb.DMatrix(as.matrix(higgs.train.dummy), 
                            label = as.numeric(higgs.labels == "X0"),
                            weight = scaled.weight)
-bst = xgb_train_1$finalModel
+xgboostTrainPred <- predict(bst, newdata = xgmat.train)
+labels <- ifelse(as.character(higgs.labels)=="X1", 1, 0)
 
-predict.train = predict(bst, xgmat.train)
-auc = roc(higgs.labels, predict.train)
+auc = roc(labels, xgboostTrainPred)
 plot(auc, print.thres=TRUE)
-auc$auc
-threshold = .996
+######## From the graph, we can tell the best threshold is 0.002
+threshold <- 0.662
+err <- mean(as.numeric(xgboostTrainPred >= threshold) != (higgs.labels == "X0"))
 
-err <- mean(as.numeric(predict.train >= threshold) != (higgs.labels == "X0"))
-
-importance_matrix = xgb.importance(feature_names = colnames(higgs.train.dummy), model = bst)
-xgb.plot.importance(importance_matrix)
-
-###################################################################
-
-xgmat.test <- xgb.DMatrix(as.matrix(dummy.data.frame(higgs.test, names = "PRI_jet_num")))
-predict <- predict(bst, xgmat.test)
-
-
-
+#########################
+#Predicting the test data
+xgmat.test <- xgb.DMatrix(as.matrix(higgs.test.dummy))
+xgboostTestPred <- predict(bst, newdata=xgmat.test)
+  
+predicted <- rep("s",550000)
+predicted[xgboostTestPred>=threshold] <- "b"
+weightRank = rank(xgboostTestPred, ties.method= "random")
+  
+submission = data.frame(EventId = higgs.testId, RankOrder = weightRank, Class = predicted)
+write.csv(submission, "xgboost_submission.csv", row.names=FALSE)
